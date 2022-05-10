9b93ea23707a8280e44dfa00af9f66e58838fb76
Sean <me@sean.taipei>
2022-05-10 16:03:29 +0800

    Add testcase: Simple Discounts

--- apps/bowling/src/app/potter.spec.ts
+++ apps/bowling/src/app/potter.spec.ts
@@ -8,4 +8,12 @@ test('Basics', () => {
   expect(potter.price([3])).toBe(8);
   expect(potter.price([4])).toBe(8);
   expect(potter.price([1, 1, 1])).toBe(8 * 3);
-})
+});
+
+test('Simple Discounts', () => {
+  const potter = new Potter();
+  expect(potter.price([0, 1])).toBe(8 * 2 * 0.95);
+  expect(potter.price([0, 2, 4])).toBe(8 * 3 * 0.9);
+  expect(potter.price([0, 1, 2, 4])).toBe(8 * 4 * 0.8);
+  expect(potter.price([0, 1, 2, 3, 4])).toBe(8 * 5 * 0.75);
+});
