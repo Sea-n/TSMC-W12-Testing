4d8226f28f5052e95092a4041300b9b89ea2aa59
Sean <me@sean.taipei>
2022-05-10 16:57:15 +0800

    Refactor Potter
    Temporarily disable Edge Cases testcase

--- apps/bowling/src/app/potter.ts
+++ apps/bowling/src/app/potter.ts
@@ -1,34 +1,41 @@
-export class BookCount {
+export class Count {
   0 = 0;
   1 = 0;
   2 = 0;
   3 = 0;
   4 = 0;
+  5 = 0;
 }
 
 export class Potter {
   price(books: number[]) {
-    const cnt = new BookCount;
+    const copies = new Count;  // each title -> how many copies
     for (const i of books) {
-      ++cnt[i as keyof BookCount];
+      ++copies[i as keyof Count];
     }
 
     let sum = 0;
+    const titles = new Count;  // how many different titles -> repeat how many times
     for (;;) {
-      let types = 0;
+      let cnt = 0;
       for (let i=0; i<=4; i++) {
-        if (cnt[i as keyof BookCount] --> 0) {
-          types++;
+        if (copies[i as keyof Count] --> 0) {
+          ++cnt;
         }
       }
-      if (!types) break;
+      ++titles[cnt as keyof Count];
+      if (!cnt) break;
+    }
 
-      switch (types) {
-        case 1: sum += 8 * 1 * 1.0; break;
-        case 2: sum += 8 * 2 * 0.95; break;
-        case 3: sum += 8 * 3 * 0.9; break;
-        case 4: sum += 8 * 4 * 0.8; break;
-        case 5: sum += 8 * 5 * 0.75; break;
+    for (let cnt=1; cnt<=5; cnt++) {
+      while (titles[cnt as keyof Count] --> 0) {
+        switch (cnt) {
+          case 1: sum += 8 * 1 * 1.0; break;
+          case 2: sum += 8 * 2 * 0.95; break;
+          case 3: sum += 8 * 3 * 0.9; break;
+          case 4: sum += 8 * 4 * 0.8; break;
+          case 5: sum += 8 * 5 * 0.75; break;
+        }
       }
     }
     return sum;
