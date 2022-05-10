1f2e11015b19bacb00bffde912bee13ee508bc41
Sean <me@sean.taipei>
2022-05-10 16:37:58 +0800

    Complete testcase: Simple Discounts

--- apps/bowling/src/app/potter.ts
+++ apps/bowling/src/app/potter.ts
@@ -1,5 +1,36 @@
+export class BookCount {
+  0 = 0;
+  1 = 0;
+  2 = 0;
+  3 = 0;
+  4 = 0;
+}
+
 export class Potter {
   price(books: number[]) {
-    return books.length * 8;
+    const cnt = new BookCount;
+    for (const i of books) {
+      ++cnt[i as keyof BookCount];
+    }
+
+    let sum = 0;
+    for (;;) {
+      let types = 0;
+      for (let i=0; i<=4; i++) {
+        if (cnt[i as keyof BookCount] --> 0) {
+          types++;
+        }
+      }
+      if (!types) break;
+
+      switch (types) {
+        case 1: sum += 8 * 1 * 1.0; break;
+        case 2: sum += 8 * 2 * 0.95; break;
+        case 3: sum += 8 * 3 * 0.9; break;
+        case 4: sum += 8 * 4 * 0.8; break;
+        case 5: sum += 8 * 5 * 0.75; break;
+      }
+    }
+    return sum;
   }
 }
